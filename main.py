e394c52ccaa3f0fbb5a6b06745df24a1d01ea172
paulo <paulo.loppes@yahoo.com>
2022-01-15 13:44:42 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import turtle
+from turtle import *
+
+turtle.title("Estrutura do Corona Virus"),
+speed(50)
+color('red')
+bgcolor('black')
+b = 200
+while b > 0:
+    left(b)
+    forward(b * 1)
+    b = b-1
+
+speed(50)
+color('cyan')
+bgcolor('black')
+b = 200
+
+while b > 0:
+    left(b)
+    forward(b * 1)
+    b = b-1
+
+
